393ba3bebda3e450fb367c13896679f0c129bcbb
Paul-Arnaud Lacroix <paularnaudlacroix@gmail.com>
2019-10-07 12:01:35 +0200

    Added QuoteCards component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import QuoteCards from './components/QuoteCards';
 
 function App() {
   return (
     <div className="App">
      <Navbar />
+     <QuoteCards />
     </div>
   );
 }
