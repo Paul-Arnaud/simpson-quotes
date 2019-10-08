babae73bf85c3250dad95425f1ed5fcae93a831a
Paul-Arnaud Lacroix <paularnaudlacroix@gmail.com>
2019-10-08 17:36:48 +0200

    added a react logo than rotate when we click on a buton (using setState)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Navbar from './components/Navbar';
 import QuoteCards from './components/QuoteCards';
+import Logo from './logo.svg';
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     <Navbar />
-     <QuoteCards />
-    </div>
-  );
-}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      working: false
+    };
+  }
+  handleClick = () => {
+    this.setState({working: !this.state.working });
+  };
+  render(){
+    const working = this.state.working ? 'App-logo App-logo-working' : 'App-logo';
+    return (
+      <div className="App">
+       <Navbar />
+       <button onClick={this.handleClick}>Rotate logo (with setState)</button>
+       <img src={Logo} alt="logo" className={working} />
+       <QuoteCards character="Homer Simpson" image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939" quote="In theory, Communism works! In theory."/>
+       <QuoteCards character="Otto" image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FOttoMann.png?1497567511803cd" quote="Oh, wow, windows. I don't think I could afford this place."/>
+      </div>
+    )
+  };
+} 
 
 export default App;
